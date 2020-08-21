5a131bbdc5e5d41de6e3ac90c5964be04fe2a79a
ddyack <ddyack@ozon.ru>
2020-08-21 08:32:22 +0300

    Initial Commit

---
+++ RGBApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RGBApp
+//
+//  Created by ddyack on 21.08.2020.
+//  Copyright © 2020 ddyack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
